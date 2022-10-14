fa86dfee4611d67ab3f70c45d929bae145b69790
Andrey <andreysyrotenko@gmail.com>
2022-10-14 14:30:12 +0300

    Task 9 fixed

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -2,13 +2,13 @@ const colorBtn = document.querySelector('.change-color');
 const colorText = document.querySelector('.color');
 const body = document.querySelector('body');
 
-body.addEventListener('click', changeColor);
+colorBtn.addEventListener('click', changeColor);
 
 function changeColor() {
   function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
   }
-
-  body.style.backgroundColor = getRandomHexColor();
-  colorText.textContent = getRandomHexColor();
+  const randomColor = getRandomHexColor();
+  body.style.backgroundColor = randomColor;
+  colorText.textContent = randomColor;
 }
