6e26de3f61e3bc8b926b16517292d02437ee2ab8
Andrey <andreysyrotenko@gmail.com>
2022-10-14 14:07:10 +0300

    Task 6 fixed

--- src/js/task-06.js
+++ src/js/task-06.js
@@ -1,12 +1,13 @@
 const passwordInput = document.querySelector('#validation-input');
+const passwordIndex = document.querySelector('#validation-input[data-length]');
 
 function passwordCheck() {
-  if (passwordInput.value.length > 1 && passwordInput.value.length < 6) {
-    passwordInput.classList.add('invalid');
-    passwordInput.classList.remove('valid');
-  } else if (passwordInput.value.length >= 6) {
+  if (passwordInput.value.length === Number(passwordInput.dataset.length)) {
     passwordInput.classList.add('valid');
     passwordInput.classList.remove('invalid');
+  } else if (passwordInput.value.length > 0) {
+    passwordInput.classList.add('invalid');
+    passwordInput.classList.remove('valid');
   } else {
     passwordInput.classList.remove('valid');
     passwordInput.classList.remove('invalid');
